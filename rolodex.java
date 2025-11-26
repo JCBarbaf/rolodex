c2da0a5b5df7225be1cfc352586828cbdaf7812e
Juan Carlos Barba <jbarbafernandez@cifpfbmoll.eu>
2025-11-26 20:56:13 +0100

    email and phone validation

--- rolodex.java
+++ rolodex.java
@@ -10,18 +10,49 @@ public class rolodex {
             System.out.println("--- Add new contact ---");
             System.out.print("-> Contact's name: ");
             String contactName = scanner.nextLine();
-            System.out.print("-> Contact's phone number: ");
-            String contactPhone = scanner.nextLine();
-            System.out.print("-> Contact's email: ");
-            String contactEmail = scanner.nextLine();
+            String contactPhone = GetPhone(scanner);
+            String contactEmail = GetEmail(scanner);
             System.out.println(contactName + "," + contactPhone + "," + contactEmail);
-            System.out.print("Do you want to add another contact?(Y,n) ");
+            System.out.print("\nDo you want to add another contact?(Y,n) ");
             String action = scanner.nextLine();
             if (action.toLowerCase().equals("n") || action.toLowerCase().equals("no")) {
                 repeat = false;
             }
         }
-        System.out.println("Goodbye! ^_^");
+        System.out.println("\nGoodbye! ^_^");
         scanner.close();
+    }
+
+    public static String GetEmail(Scanner scanner) {
+        boolean repeat = true;
+        String contactEmail = "";
+        while (repeat) {
+            System.out.print("-> Contact's email: ");
+            contactEmail = scanner.nextLine();
+            if (contactEmail.matches("^[\\w.!#$%&'*+/=?`{|}~^-]+@[\\w.-]+$")) {
+                repeat = false;
+            } else {
+                System.out.println("X_X Error: Invalid email");
+            }
+            
+        }
+        return contactEmail;
+    }
+
+    public static String GetPhone(Scanner scanner) {
+        boolean repeat = true;
+        String contactPhone = "";
+        while (repeat) {
+            System.out.print("-> Contact's Phone number: ");
+            contactPhone = scanner.nextLine();
+            contactPhone = contactPhone.replace(" ", "");
+            if (contactPhone.matches("^\\+?[1-9][0-9]{7,14}$")) {
+                repeat = false;
+            } else {
+                System.out.println("X_X Error: Invalid phone number");
+            }
+            
+        }
+        return contactPhone;
     }
 }
