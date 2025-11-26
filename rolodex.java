0853ef20d660d4660ef6d52fae5fbeb67543c7a3
Juan Carlos Barba <jbarbafernandez@cifpfbmoll.eu>
2025-11-26 20:19:22 +0100

    loop

--- rolodex.java
+++ rolodex.java
@@ -6,15 +6,22 @@ public class rolodex {
         boolean repeat = true;
         System.out.println("Wellcome to rolodex");
         Scanner scanner = new Scanner(System.in);
-        
-
-        System.out.print("-> Contact's name: ");
-        String contactName = scanner.nextLine();
-        System.out.print("-> Contact's phone number: ");
-        String contactPhone = scanner.nextLine();
-        System.out.print("-> Contact's email: ");
-        String contactEmail = scanner.nextLine();
+        while (repeat) { 
+            System.out.println("--- Add new contact ---");
+            System.out.print("-> Contact's name: ");
+            String contactName = scanner.nextLine();
+            System.out.print("-> Contact's phone number: ");
+            String contactPhone = scanner.nextLine();
+            System.out.print("-> Contact's email: ");
+            String contactEmail = scanner.nextLine();
+            System.out.println(contactName + "," + contactPhone + "," + contactEmail);
+            System.out.print("Do you want to add another contact?(Y,n) ");
+            String action = scanner.nextLine();
+            if (action.toLowerCase().equals("n") || action.toLowerCase().equals("no")) {
+                repeat = false;
+            }
+        }
+        System.out.println("Goodbye! ^_^");
         scanner.close();
-        System.out.println(contactName + "," + contactPhone + "," + contactEmail);
     }
 }
