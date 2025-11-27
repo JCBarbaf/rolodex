362450af95d8f88dc29fb4804120e4e32ecb9559
Juan Carlos Barba <jbarbafernandez@cifpfbmoll.eu>
2025-11-27 20:22:32 +0100

    file handling

--- rolodex.java
+++ rolodex.java
@@ -1,18 +1,44 @@
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class rolodex {
     public static void main(String[] args) {
         boolean repeat = true;
-        System.out.println("Wellcome to rolodex");
+        System.out.println("\nWellcome to rolodex");
+        File csvFile = new File("rolodex.csv");
+        if (!csvFile.exists()) {
+            System.out.println("\n-> No CSV file detected. Creating a new CSV file.");
+            try {
+                csvFile.createNewFile();
+                System.out.println("CSV file created! ^_^");
+            } catch (Exception e) {
+                System.out.println("Error creating the file: " + e.getMessage());
+            }
+        }
         Scanner scanner = new Scanner(System.in);
         while (repeat) { 
-            System.out.println("--- Add new contact ---");
+            System.out.println("\n--- Add new contact ---");
             System.out.print("-> Contact's name: ");
             String contactName = scanner.nextLine();
             String contactPhone = GetPhone(scanner);
             String contactEmail = GetEmail(scanner);
-            System.out.println(contactName + "," + contactPhone + "," + contactEmail);
+            try {
+                Scanner reader = new Scanner(csvFile);
+                String fileContent = reader.nextLine();
+                while (reader.hasNextLine()) {
+                    fileContent += "\n" + reader.nextLine();
+                }
+                reader.close();
+                FileWriter myWriter = new FileWriter("rolodex.csv");
+                fileContent += "\n" + contactName + "," + contactPhone + "," + contactEmail;
+                myWriter.write(fileContent);
+                myWriter.close();
+                System.out.println("\nContact added successfully! ^_^");
+            } catch (Exception e) {
+                System.out.println("Error adding contact: " + e.getMessage());
+            }
             System.out.print("\nDo you want to add another contact?(Y,n) ");
             String action = scanner.nextLine();
             if (action.toLowerCase().equals("n") || action.toLowerCase().equals("no")) {
