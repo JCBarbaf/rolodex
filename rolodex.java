7904304ca076c24b07f918f05c1267b3a8c05460
Juan Carlos Barba <jbarbafernandez@cifpfbmoll.eu>
2025-11-28 16:33:38 +0100

    csvfile final touches

--- rolodex.java
+++ rolodex.java
@@ -12,6 +12,9 @@ public class rolodex {
             System.out.println("\n-> No CSV file detected. Creating a new CSV file.");
             try {
                 csvFile.createNewFile();
+                FileWriter myWriter = new FileWriter(csvFile);
+                myWriter.write("Name,Phone number,Email");
+                myWriter.close();
                 System.out.println("CSV file created! ^_^");
             } catch (Exception e) {
                 System.out.println("Error creating the file: " + e.getMessage());
@@ -25,16 +28,7 @@ public class rolodex {
             String contactPhone = GetPhone(scanner);
             String contactEmail = GetEmail(scanner);
             try {
-                Scanner reader = new Scanner(csvFile);
-                String fileContent = reader.nextLine();
-                while (reader.hasNextLine()) {
-                    fileContent += "\n" + reader.nextLine();
-                }
-                reader.close();
-                FileWriter myWriter = new FileWriter("rolodex.csv");
-                fileContent += "\n" + contactName + "," + contactPhone + "," + contactEmail;
-                myWriter.write(fileContent);
-                myWriter.close();
+                AddNewLine(csvFile, "\n" + contactName + "," + contactPhone + "," + contactEmail);
                 System.out.println("\nContact added successfully! ^_^");
             } catch (Exception e) {
                 System.out.println("Error adding contact: " + e.getMessage());
@@ -80,5 +74,22 @@ public class rolodex {
             
         }
         return contactPhone;
+    }
+
+    public static void AddNewLine(File file, String newLine) throws Exception {
+        try {
+            Scanner reader = new Scanner(file);
+            String fileContent = reader.nextLine();
+            while (reader.hasNextLine()) {
+                fileContent += "\n" + reader.nextLine();
+            }
+            reader.close();
+            FileWriter myWriter = new FileWriter(file);
+            fileContent += newLine;
+            myWriter.write(fileContent);
+            myWriter.close();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
